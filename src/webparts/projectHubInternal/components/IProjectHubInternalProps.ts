c20249abdd95334e8d54332befc51db044b09718
aravindhvetri <2002aravinthv@gmail.com>
2025-11-11 18:06:48 +0530

    aari's first push to main branch

---
+++ src/webparts/projectHubInternal/components/IProjectHubInternalProps.ts
@@ -0,0 +1,7 @@
+export interface IProjectHubInternalProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/projectHubInternal/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IProjectHubInternalWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ProjectHubInternalWebPartStrings' {
+  const strings: IProjectHubInternalWebPartStrings;
+  export = strings;
+}
